18970f13d778987fbafbbf196162d07e2b852da0
Juan Angel <juangel2406@gmail.com>
2020-06-30 20:17:28 -0500

    fue culpa de Mario

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/http/user.service';
 import { Router } from '@angular/router';
 import { LoteriaService } from 'src/app/services/ws/loteria.service';
-import Ws from '@adonisjs/websocket-client';
 
 @Component({
   selector: 'app-navbar',
@@ -22,7 +21,7 @@ export class NavbarComponent implements OnInit {
 
   logout() {
     let user = JSON.parse(sessionStorage.getItem('user'))
-    this.socket.emitClose(user.id)
+    this.loteriaService.emitClose(user.id)
 
     sessionStorage.removeItem('user')
     this.userService.logout()
